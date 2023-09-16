418804bb7579f01aa7725cc5167adf598b7b6342
nimixx <nimixx.dev@proton.me>
2023-09-17 00:23:13 +0200

    Edit format setup

--- src/consts.ts
+++ src/consts.ts
@@ -2,13 +2,14 @@
 export const SITE_TITLE = 'Nimixx.dev'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
 export const CREATOR_AUTHOR_NAME = 'Tadeáš Thelen'
+export const ACTUAL_YEAR = new Date().getFullYear()
 
 //Image authors
-export const AUTHOR_HERO = 'pikisuperstar'
+export const AUTHOR_HERO = 'Freepik'
 
 //Image sources
 export const SOURCE_HERO =
-	'https://www.freepik.com/free-vector/gradient-top-view-laptop-technology-background_4132603.htm#query=developer%20wallpaper&position=0&from_view=search&track=ais'
+  'https://www.freepik.com/free-vector/gradient-abstract-wireframe-background_15592515.htm#page=3&query=developer%20wallpaper&position=10&from_view=search&track=ais'
 
 //Url constants
 export const HOME = '/'
@@ -16,11 +17,38 @@ export const PROJECT = '/project'
 export const BLOG = '/blog'
 export const BIO = '/bio'
 
-//Navigation
-
+//Navigation items
 export const NAV_ITEMS = [
-	{ label: 'Domů', path: HOME },
-	{ label: 'Projekty', path: PROJECT },
-	{ label: 'Blog', path: BLOG },
-	{ label: 'Bio', path: BIO }
+  { label: 'Domů', path: HOME },
+  { label: 'Projekty', path: PROJECT },
+  { label: 'Blog', path: BLOG },
+  { label: 'Bio', path: BIO }
 ]
+
+//Social media
+
+export const SOCIAL_LINKS = [
+  {
+    name: 'GitHub',
+    href: 'https://github.com/Nimixx',
+    icon: 'simple-icons:github'
+  },
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/NimixxDev',
+    icon: 'simple-icons:twitter'
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/tadeasthelen/',
+    icon: 'simple-icons:linkedin'
+  },
+  {
+    name: 'YouTube',
+    href: '/',
+    icon: 'simple-icons:youtube'
+  }
+]
+
+//Mix constants
+export const COPYRIGHT = `© ${ACTUAL_YEAR} ${CREATOR_AUTHOR_NAME}`
