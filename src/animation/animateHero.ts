5c901d2946cde8c08858e5f8cd552c9202ab724a
nimixx <nimixx.dev@proton.me>
2023-09-24 23:50:21 +0200

    Add new hero animation

--- src/animation/animateHero.ts
+++ src/animation/animateHero.ts
@@ -1,22 +1,31 @@
 import { gsap } from 'gsap'
 
-// Create a GSAP timeline
+type AnimationProps = {
+  opacity?: number
+  top?: string | number
+  right?: string | number
+  rotate?: string | number
+}
+
+function animateElement(
+  target: string,
+  props: AnimationProps,
+  duration = 1,
+  delay = 0,
+  ease = 'Power4.easeNone'
+) {
+  return gsap.to(target, {
+    opacity: 1,
+    duration,
+    delay,
+    ease,
+    ...props
+  })
+}
+
 const tl = gsap.timeline()
 
-// Add animations to the timeline
-tl.to('.hero_img', {
-  translateY: '0%',
-  duration: 1.5,
-  delay: 0.5,
-  ease: 'Bounce.easeOut'
-})
-.to(
-  ['.hero_title', '.hero_author'], // Use an array to select multiple elements
-  {
-    opacity: 1,
-    duration: 1,
-    ease: 'Power0.easeNone'
-  },
-  '+=0.2'
-)
-
+tl.add(animateElement('.base_layer', {}), 0.5)
+tl.add(animateElement('.rectangle_1', { top: '0' }), '-=1')
+tl.add(animateElement('.rectangle_2', { right: '0' }), '-=1')
+tl.add(animateElement('.circle', { top: '0', rotate: '0' }), '-=1')
